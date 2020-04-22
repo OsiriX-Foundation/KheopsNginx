77714f108ac562dc0702a6e33cdfc9f1c4c4a429
gerome12 <gerome.pasquier@gmail.com>
2020-04-22 17:32:00 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -88,11 +88,28 @@ else
    echo -e "all secrets and all env var \e[92mOK\e[0m"
 fi
 
+
+# extract the protocol
+proto="$(echo $KHEOPS_ROOT_URL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+# remove the protocol
+url="$(echo ${KHEOPS_ROOT_URL/$proto/})"
+# extract the user (if any)
+user="$(echo $url | grep @ | cut -d@ -f1)"
+# extract the host and port
+hostport="$(echo ${url/$user@/} | cut -d/ -f1)"
+# by request host without port    
+host="$(echo $hostport | sed -e 's,:.*,,g')"
+# by request - try to extract the port
+port="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+# extract the path (if any)
+path="$(echo $url | grep / | cut -d/ -f2-)"
+
+
 roothost="$(awk -F/ '{sub("^[^@]+@","",$3); print $3}' <<<$KHEOPS_ROOT_URL)"
 
 #get env var
 chmod a+w /etc/nginx/conf.d/kheops.conf
-sed -i "s|\${root_url}|$KHEOPS_ROOT_URL|" /etc/nginx/conf.d/kheops.conf
+sed -i "s|\${root_url}|$proto://$host|" /etc/nginx/conf.d/kheops.conf
 
 sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_DICOMWEB_PROXY_HOST:$KHEOPS_DICOMWEB_PROXY_PORT|" /etc/nginx/conf.d/kheops.conf
 sed -i "s|\${kheopsAuthorization_url}|http://$KHEOPS_AUTHORIZATION_HOST:$KHEOPS_AUTHORIZATION_PORT|" /etc/nginx/conf.d/kheops.conf
