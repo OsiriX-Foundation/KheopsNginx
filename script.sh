f5adbb29cbd6b54a8660c9362ba49203c970dfda
gerome12 <gerome.pasquier@gmail.com>
2020-10-19 13:26:14 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -129,16 +129,16 @@ sed -i "s|\${kheopsWebUI_url}|http://$KHEOPS_UI_HOST:$KHEOPS_UI_PORT|" /etc/ngin
 
 sed -i "s|\${server_name}|$roothost|" /etc/nginx/locations.conf
 
-chmod a+w /etc/nginx/dev_locations.conf
-sed -i "s|\${root_url}|$proto$host|" /etc/nginx/dev_locations.conf
+chmod a+w /etc/nginx/dev_location.conf
+sed -i "s|\${root_url}|$proto$host|" /etc/nginx/dev_location.conf
 
-sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_DICOMWEB_PROXY_HOST:$KHEOPS_DICOMWEB_PROXY_PORT|" /etc/nginx/dev_locations.conf
-sed -i "s|\${kheopsAuthorization_url}|http://$KHEOPS_AUTHORIZATION_HOST:$KHEOPS_AUTHORIZATION_PORT|" /etc/nginx/dev_locations.conf
-sed -i "s|\${kheopsAuthorizationProxy_url}|http://$KHEOPS_PACS_PEP_HOST:$KHEOPS_PACS_PEP_PORT|" /etc/nginx/dev_locations.conf
-sed -i "s|\${kheopsZipper_url}|http://$KHEOPS_ZIPPER_HOST:$KHEOPS_ZIPPER_PORT|" /etc/nginx/dev_locations.conf
-sed -i "s|\${kheopsWebUI_url}|http://$KHEOPS_UI_HOST:$KHEOPS_UI_PORT|" /etc/nginx/dev_locations.conf
+sed -i "s|\${DICOMWebProxy_url}|$KHEOPS_DICOMWEB_PROXY_HOST:$KHEOPS_DICOMWEB_PROXY_PORT|" /etc/nginx/dev_location.conf
+sed -i "s|\${kheopsAuthorization_url}|http://$KHEOPS_AUTHORIZATION_HOST:$KHEOPS_AUTHORIZATION_PORT|" /etc/nginx/dev_location.conf
+sed -i "s|\${kheopsAuthorizationProxy_url}|http://$KHEOPS_PACS_PEP_HOST:$KHEOPS_PACS_PEP_PORT|" /etc/nginx/dev_location.conf
+sed -i "s|\${kheopsZipper_url}|http://$KHEOPS_ZIPPER_HOST:$KHEOPS_ZIPPER_PORT|" /etc/nginx/dev_location.conf
+sed -i "s|\${kheopsWebUI_url}|http://$KHEOPS_UI_HOST:$KHEOPS_UI_PORT|" /etc/nginx/dev_location.conf
 
-sed -i "s|\${server_name}|$roothost|" /etc/nginx/dev_locations.conf
+sed -i "s|\${server_name}|$roothost|" /etc/nginx/dev_location.conf
 
 echo "Ending setup secrets and env var"
 
